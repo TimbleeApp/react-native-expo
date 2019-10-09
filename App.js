11f598441ffe5018a27ddbcd1a8ce16cd774c7eb
Semal <semal@orangebicycle.co>
2019-10-09 12:48:22 +0530

    updated code

--- App.js
+++ App.js
@@ -1,40 +1,24 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import React, { Component } from 'react';
+import {StyleSheet, View, StatusBar } from 'react-native';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
+import Routes from './src/Routes';
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-export default class App extends React.Component {
+export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Semal here is your name for first time on device
-        </Text>
-        <Card>
-          <AssetExample />
-        </Card>
+        <StatusBar
+           backgroundColor="#1c313a"
+           barStyle="light-content"
+         />
+        <Routes/>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container : {
     flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+  }
 });
